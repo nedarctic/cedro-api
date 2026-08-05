3ee164f4d67e6554298a5f176ddef15db25ea822
Ned Arctic <nedarctic@gmail.com>
2026-08-05 14:13:14 +0300

    dto updates

--- src/tours/dto/update-tour.dto.ts
+++ src/tours/dto/update-tour.dto.ts
@@ -2,7 +2,6 @@ import { Type } from "class-transformer";
 import { IsArray, IsInt, IsOptional, IsString, IsUUID, ValidateNested } from "class-validator";
 
 export class ItineraryDto {
-    @IsOptional()
     @IsUUID()    
     id?: string;
 
@@ -18,37 +17,48 @@ export class ItineraryDto {
     activities!: string[];
 }
 
-export class CreateTourDto {
+export class UpdateTourDto {
+    @IsOptional()
     @IsString()
     dates!: string;
     
+    @IsOptional()
     @IsString()
     duration!: string;
     
+    @IsOptional()
     @Type(() => Number)
     groupSize!: number;
     
+    @IsOptional()
     @Type(() => Number)
     price!: number;
     
+    @IsOptional()
     @IsString()
     title!: string;
     
+    @IsOptional()
     @IsString()
     intro!: string;
     
+    @IsOptional()
     @IsArray()
     included!: string[];
     
+    @IsOptional()
     @IsArray()
     excluded!: string[];
     
+    @IsOptional()
     @IsArray()
     activities!: string[];
 
+    @IsOptional()
     @IsString()
     destinationId!: string;
 
+    @IsOptional()
     @IsArray()
     @ValidateNested({each: true})
     @Type(() => ItineraryDto)
