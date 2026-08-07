b68590b26d6a7ef00eec91030b27ab5094d2418c
Ned Arctic <nedarctic@gmail.com>
2026-08-07 16:45:29 +0300

    anonymous story exception

--- src/blogs/exceptions/story-not-found.exception.ts
+++ src/blogs/exceptions/story-not-found.exception.ts
@@ -1,7 +1,7 @@
 import { NotFoundException } from "@nestjs/common";
 
 export class StoryNotFoundException extends NotFoundException {
-    constructor(storyId: string) {
-        super(`Story with ID ${storyId} not found`);
+    constructor() {
+        super("Story not found.");
     }
 }
