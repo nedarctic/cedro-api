2ff3d5e7d2eb1aeab2813f9092f95c2655bb1d87
Ned Arctic <nedarctic@gmail.com>
2026-08-07 16:44:58 +0300

    manual blog create data validation

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -6,6 +6,10 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { UserRole } from '../generated/prisma/enums';
 import { BlogsService } from './blogs.service';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { plainToInstance } from 'class-transformer';
+import { CreateBlogDto } from './dto/create-blog.dto';
+import { validateOrReject } from 'class-validator';
+import { UpdateBlogDto } from './dto/update-blog.dto';
 
 
 @Controller('blogs')
@@ -18,10 +22,11 @@ export class BlogsController {
     @UseInterceptors(FileInterceptor('image'))
     async createBlog(
         @UploadedFile() image: Express.Multer.File,
-        @Body() body: { title: string; date: string; excerpt: string }
+        @Body() body: { blog: string }
     ) {
-        const { title, date, excerpt } = body;
-        return this.blogsService.createBlog(title, date, excerpt, image);
+        const blog = plainToInstance(CreateBlogDto, JSON.parse(body.blog));
+        await validateOrReject(blog);
+        return await this.blogsService.createBlog(blog, image);
     }
 
     @Get()
@@ -48,60 +53,10 @@ export class BlogsController {
     async updateBlog(
         @Param('id') id: string,
         @UploadedFile() image: Express.Multer.File,
-        @Body() body: { title?: string; date?: string; excerpt?: string }
+        @Body() body: { blog: string }
     ) {
-        const { title, date, excerpt } = body;
-        return this.blogsService.updateBlog(id, { title, date, excerpt, image });
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Post(':id/story')
-    async createStoryForBlog(@Param('id') id: string, @Body() body: { intro: string; conclusion: string }) {
-        const { intro, conclusion } = body;
-        return this.blogsService.addStoryToBlog(id, intro, conclusion);
-    }
-
-    @Get(':id/story')
-    async getStoryByBlogId(@Param('id') id: string) {
-        return this.blogsService.getStoryByBlogId(id);
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Patch('story/:id')
-    async updateBlogStory(@Param('id') id: string, @Body() body: { intro?: string; conclusion?: string }) {
-        const { intro, conclusion } = body;
-        return this.blogsService.updateBlogStory(id, { intro, conclusion });
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Delete('story/:id')
-    async deleteBlogStory(@Param('id') id: string) {
-        return this.blogsService.deleteBlogStory(id);
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Post('story/:id/section')
-    async addSectionToStory(@Param('id') id: string, @Body() body: { subtitle: string; content: string }) {
-        const { subtitle, content } = body;
-        return this.blogsService.addSectionToStory(id, subtitle, content);
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Patch('story/:storyId/section/:sectionId')
-    async updateStorySection(@Param('storyId') storyId: string, @Param('sectionId') sectionId: string, @Body() body: { subtitle?: string; content?: string }) {
-        const { subtitle, content } = body;
-        return this.blogsService.updateStorySection(storyId, sectionId, { subtitle, content });
-    }
-
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(UserRole.SUPER_ADMIN)
-    @Delete('story/:storyId/section/:sectionId')
-    async deleteStorySection(@Param('storyId') storyId: string, @Param('sectionId') sectionId: string) {
-        return this.blogsService.deleteStorySection(storyId, sectionId);
-    }
+        const blogData = plainToInstance(UpdateBlogDto, JSON.parse(body.blog));
+        await validateOrReject(blogData);
+        return this.blogsService.updateBlog(id, blogData, image);
+    }    
 }
