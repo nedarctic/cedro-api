bb7a32cb15e3fc04f499dfab2bd853e8ea25591b
Ned Arctic <nedarctic@gmail.com>
2026-08-13 23:49:20 +0300

    added mail module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BlogsModule } from './blogs/blogs.module';
+import { BookingsModule } from './bookings/bookings.module';
 import configuration from './config/configuration';
 import { validationSchema } from './config/validation';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { AuthModule } from './auth/auth.module';
-import { ToursModule } from './tours/tours.module';
-import { BlogsModule } from './blogs/blogs.module';
-import { R2Module } from './r2/r2.module';
-import { TestimonialsModule } from './testimonials/testimonials.module';
+import { DashModule } from './dash/dash.module';
+import { DestinationsModule } from './destinations/destinations.module';
 import { FaqsModule } from './faqs/faqs.module';
 import { ItinerariesModule } from './itineraries/itineraries.module';
-import { TeamModule } from './team/team.module';
-import { BookingsModule } from './bookings/bookings.module';
-import { DestinationsModule } from './destinations/destinations.module';
-import { DashModule } from './dash/dash.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { MailModule } from './mail/mail.module';
 import { MessagesModule } from './messages/messages.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { R2Module } from './r2/r2.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { TeamModule } from './team/team.module';
+import { TestimonialsModule } from './testimonials/testimonials.module';
+import { ToursModule } from './tours/tours.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -49,6 +50,7 @@ import { MessagesModule } from './messages/messages.module';
     DashModule,
     SubscriptionsModule,
     MessagesModule,
+    MailModule,
   ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
